b9e9e131a8039c8c8fa000d5f1619d0ae3399679
Joao Moraes <joaomoraesbr@gmail.com>
2018-09-02 02:15:38 -0300

    more code

--- src/Strategies/MySQLStrategy.php
+++ src/Strategies/MySQLStrategy.php
@@ -2,9 +2,10 @@
 
 namespace FcPhp\Datasource\MySQL\Strategies
 {
+    use FcPhp\Datasource\Strategy;
     use FcPhp\Datasource\Interfaces\IStrategy;
 
-    class MySQL implements IStrategy
+    class MySQL extends Strategy implements IStrategy
     {
         private $select = [];
         private $table;
@@ -26,28 +27,66 @@ namespace FcPhp\Datasource\MySQL\Strategies
             return $this;
         }
 
-        public function join(string $join, string $field, string $condition, string $value)
+        public function join(string $joinType, string $field, string $condition, string $value)
         {
-            $this->join[] = compact('join', 'field', 'condition', 'value');
+            $this->join[] = compact('joinType', 'field', 'condition', 'value');
             return $this;
         }
 
-        public function or(object $clousure)
+        public function where(object $callback)
         {
-            // $this->where[] = ['or' => $clousure($this)];
-            return $this;
-        }
-
-        public function and(object $clousure)
-        {
-            // $this->where[] = ['and' => $clousure($this)];
-            return $this;
-        }
-
-        public function where(string $field, string $condition, string $value)
-        {
-            // $this->where[] = ['and' => compact('field', 'condition', 'value')];
+            $criteria = $this->getCriteria();
+            $callback($criteria);
+            $this->where = array_merge($this->where, $criteria->getWhere());
             return $this;
         }
     }
 }
+
+// $this->where(function(ICriteria $criteria) {
+//     $criteria->or(function(ICriteria $criteria) {
+//         $criteria->condition('field', 'condition', 'value');
+//         $criteria->condition('field2', 'condition2', 'value2');
+//         $criteria->and(function(ICriteria $criteria) {
+//             $criteria->condition('field3', 'condition3', 'value3');
+//             $criteria->condition('field4', 'condition4', 'value4');
+//         });
+//     });
+//     $criteria->and(function(ICriteria $criteria) {
+//         $criteria->condition('field5', 'condition5', 'value5');
+//         $criteria->condition('field6', 'condition6', 'value6');
+//     });
+// });
+
+// [
+//     'OR',
+//     [
+//         'AND',
+//         [
+//             'o.item like "test%"',
+//             'c.active=1',
+//         ],
+//         'OR',
+//         [
+//             'o.item like "test%"',
+//             'c.active=1',
+//         ],
+//         'o.item like "test%"',
+//     ],
+//     'AND',
+//     [
+//         'OR',
+//         [
+//             '',
+//             '',
+//         ],
+//         'AND',
+//         [
+//             'OR',
+//             [
+//                 '',
+//                 ''
+//             ],
+//         ]
+//     ]
+// ]
